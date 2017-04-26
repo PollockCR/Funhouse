84aa47d88b71c2972bae34f60208f63df2357b9c
Catherine Pollock <cpollock@unr.edu>
2017-04-25 17:19:45 -0700

    trying to FPS

--- BiggestLittleFunhouse/Assets/Scripts/FirstPersonController.cs
+++ BiggestLittleFunhouse/Assets/Scripts/FirstPersonController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour {
 
+	public float movementSpeed = 10.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +14,14 @@ public class FirstPersonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+
+
+		Vector3 speed = new Vector3 (sideSpeed, 0, forwardSpeed);
+
+		CharacterController cc = GetComponent<CharacterController> ();
+
+		cc.SimpleMove (speed);
 	}
 }
